442464668ee633bf3d6e94b95e9fc87cf16464a7
d.rakhamnazhov <dastan.rakhamnazhov@timelysoft.net>
2022-05-25 00:42:10 +0600

    Update env url

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   proxy: true,
-  url: env("https://fierce-temple-38026.herokuapp.com/"),
+  url: env("MY_HEROKU_URL"),
   app: {
     keys: env.array("APP_KEYS"),
   },
